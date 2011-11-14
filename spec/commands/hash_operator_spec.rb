b72be90a8d75d0164c87777a5705fbaf82422f17
E.J. Finneran <ej.finneran@gmail.com>
2011-11-14 12:15:34 -0600

    Support redisrb's hash operator for get/set
    Change-Id: I0f95aa406c6606382245c6482aa2ef70e0bb4174
    Reviewed-on: https://gerrit.causes.com/1525
    Tested-by: Greg Hurrell <greg@causes.com>
    Reviewed-by: Shane da Silva <shane@causes.com>

---
+++ spec/commands/hash_operator_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+
+describe "#[](key)" do
+  before do
+    @key = 'mock-redis-test:hash_operator'
+  end
+
+  it "returns nil for a nonexistent value" do
+    @redises['mock-redis-test:does-not-exist'].should be_nil
+  end
+
+  it "returns a stored string value" do
+    @redises[@key] = 'forsooth'
+    @redises[@key].should == 'forsooth'
+  end
+
+  it "treats integers as strings" do
+    @redises[@key] = 100
+    @redises[@key].should == "100"
+  end
+end
