e551ffa3517e5d82b4bfeb93ccbe876162ae5880
Julian Doherty <julian@envato.com>
2012-08-20 11:56:26 +1000

    spec behaviour of operations done in pipelines

--- spec/commands/pipelined_spec.rb
+++ spec/commands/pipelined_spec.rb
@@ -8,4 +8,16 @@ describe '#pipelined' do
     end
     res.should == true
   end
+
+  it "returns results of pipelined operations" do
+    @redises.set "hello", "world"
+    @redises.set "foo", "bar"
+
+    results = @redises.pipelined do
+      @redises.get "hello"
+      @redises.get "foo"
+    end
+
+    results.should == ["world", "bar"]
+  end
 end
