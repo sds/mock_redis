acc117d95491c94ebf87462c67f325a53d7b2dfd
Dirk Breuer <dirk.breuer@gmail.com>
2012-09-10 15:23:30 +0200

    Make `sadd` Redis 2.4 compliant
    Change-Id: Icc8bc346f4e2c91f870023e39850899b63830e61
    Reviewed-on: https://gerrit.causes.com/12084
    Tested-by: jenkins <jenkins@causes.com>
    Reviewed-by: Aiden Scandella <aiden@causes.com>

--- spec/commands/sadd_spec.rb
+++ spec/commands/sadd_spec.rb
@@ -18,5 +18,24 @@ describe '#sadd(key, member)' do
     @redises.smembers(@key).should == %w[2 1]
   end
 
+  describe 'adding multiple members at once' do
+
+    it "returns the amount of added members" do
+      @redises.sadd(@key, [1, 2, 3]).should == 3
+      @redises.sadd(@key, [1, 2, 3, 4]).should == 1
+    end
+
+    it "returns 0 if the set did already contain all members" do
+      @redises.sadd(@key, [1, 2, 3])
+      @redises.sadd(@key, [1, 2, 3]).should == 0
+    end
+
+    it "adds the members to the set" do
+      @redises.sadd(@key, [1, 2, 3])
+      @redises.smembers(@key).should == %w[1 2 3]
+    end
+
+  end
+
   it_should_behave_like "a set-only command"
 end
