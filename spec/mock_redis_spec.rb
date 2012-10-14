92135849be134c82537ba289a76099ad15b4e34f
Aiden Scandella <aiden@causes.com>
2012-10-14 10:46:02 -0700

    Clean up connection specs
    - break test into its two distinct components: `.new` and `.connect`
    - use `subject` and `its` blocks where appropriate
    - use `let` syntax to de-dupe connection strings
    - separate `#id` from `#location` tests
    
    Change-Id: Id2da12d6e92543b59ce5f2a0e820fe73f805a12d
    Reviewed-on: https://gerrit.causes.com/13593
    Tested-by: jenkins <jenkins@causes.com>
    Reviewed-by: Aiden Scandella <aiden@causes.com>

--- spec/mock_redis_spec.rb
+++ spec/mock_redis_spec.rb
@@ -1,39 +1,32 @@
 require 'spec_helper'
 
 describe MockRedis do
+  let(:url) { 'redis://127.0.0.1:6379/1' }
 
-  context "with passed options" do
-    before do
-      @mock = MockRedis.new(:url => "redis://127.0.0.1:6379/1")
+  describe '.new' do
+    subject { MockRedis.new(:url => url) }
+
+    it 'correctly parses options' do
+      subject.host.should == '127.0.0.1'
+      subject.port.should == 6379
+      subject.db.should == 1
     end
 
-    it "should correctly parse options" do
-      @mock.host.should == "127.0.0.1"
-      @mock.port.should == 6379
-      @mock.db.should == 1
-    end
+    its(:id) { should == url }
 
-    it "should have an id and location equal to redis url" do
-      @mock.id.should == "redis://127.0.0.1:6379/1"
-      @mock.location.should == "redis://127.0.0.1:6379/1"
-    end
-
-    context "when connecting to redis" do
-      before do
-        @mock = MockRedis.connect(:url => "redis://127.0.0.1:6379/0")
-      end
-
-      it "should correctly parse options" do
-        @mock.host.should == "127.0.0.1"
-        @mock.port.should == 6379
-        @mock.db.should == 0
-      end
-
-      it "should have an id equal to redis url" do
-        @mock.id.should == "redis://127.0.0.1:6379/0"
-        @mock.location.should == "redis://127.0.0.1:6379/0"
-      end
-    end
+    its(:location) { should == url }
   end
 
+  describe '.connect' do
+    let(:url) { 'redis://127.0.0.1:6379/0' }
+    subject { MockRedis.connect(:url => url) }
+
+    it 'correctly parses options' do
+      subject.host.should == '127.0.0.1'
+      subject.port.should == 6379
+      subject.db.should == 0
+    end
+
+    its(:id) { should == url }
+  end
 end
