c6de05a83b8ffee731b4b628d53abd252fe0f10b
Eoin Coffey <ecoffey@gmail.com>
2012-10-27 15:12:58 -0600

    Use more fluent "it" descriptions

--- spec/support/shared_examples/sorts_enumerables.rb
+++ spec/support/shared_examples/sorts_enumerables.rb
@@ -4,51 +4,51 @@ shared_examples_for "a sortable" do
   end
 
   context 'ordering' do
-    it 'ascending by default' do
+    it 'orders ascending by default' do
       @redises.sort(@key).should == ['1', '2']
     end
 
-    it 'descending' do
+    it 'orders by descending when specified' do
       @redises.sort(@key, :order => "DESC").should == ['2', '1']
     end
   end
 
   context 'projections' do
-    it ':get => "#"' do
+    it 'projects element when :get is "#"' do
       @redises.sort(@key, :get => '#').should == ['1', '2']
     end
 
-    it ':get => "values_*"' do
+    it 'projects through a key pattern' do
       @redises.sort(@key, :get => 'mock-redis-test:values_*').should == ['a', 'b']
     end
 
-    it ':get => ["#", "values_*"]' do
+    it 'projects through a key pattern and reflects element' do
       @redises.sort(@key, :get => ['#', 'mock-redis-test:values_*']).should == [['1', 'a'], ['2', 'b']]
     end
 
-    it ':get => "hash_*->key"' do
+    it 'projects through a hash key pattern' do
       @redises.sort(@key, :get => 'mock-redis-test:hash_*->key').should == ['x', 'y']
     end
   end
 
   context 'weights' do
-    it ':by => "weight_*"' do
+    it 'weights by projecting through a key pattern' do
       @redises.sort(@key, :by => "mock-redis-test:weight_*").should == ['2', '1']
     end
 
-    it ':order => desc' do
+    it 'weights by projecting through a key pattern and a specific order' do
       @redises.sort(@key, :order => "DESC", :by => "mock-redis-test:weight_*").should == ['1', '2']
     end
   end
 
   context 'limit' do
-    it ':limit => [offest, count]' do
+    it 'only returns requested window in the enumerable' do
       @redises.sort(@key, :limit => [0, 1]).should == ['1']
     end
   end
 
   context 'store' do
-    it ':store => "some_bucket"' do
+    it 'stores into another key' do
       @redises.sort(@key, :store => "mock-redis-test:some_bucket").should == 2
       @redises.lrange("mock-redis-test:some_bucket", 0, -1).should == ['1', '2']
     end
