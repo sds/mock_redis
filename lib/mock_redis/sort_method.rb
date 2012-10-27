38f9b6a46240c0478d8255f167636ba1a402bba2
Eoin Coffey <ecoffey@gmail.com>
2012-10-27 15:11:56 -0600

    Break up sort method into composable methods

--- lib/mock_redis/sort_method.rb
+++ lib/mock_redis/sort_method.rb
@@ -1,10 +1,8 @@
 require 'mock_redis/assertions'
-require 'mock_redis/utility_methods'
 
 class MockRedis
   module SortMethod
     include Assertions
-    include UtilityMethods
 
     def sort(key, options={})
       return [] if key.nil?
@@ -20,6 +18,33 @@ class MockRedis
       order = options[:order] || "ASC"
       direction = order.split.first
 
+      projected = project(enumerable, by, get_patterns)
+      sorted = sort_by(projected, direction)
+      sliced = slice(sorted, limit)
+
+      store ? rpush(store, sliced) : sliced
+    end
+
+    private
+
+    def project(enumerable, by, get_patterns)
+      enumerable.map do |*elements|
+        element = elements.last
+        weight = by ? lookup_from_pattern(by, element) : element
+        value = element
+
+        if get_patterns.length > 0
+          value = get_patterns.map do |pattern|
+            pattern == "#" ? element : lookup_from_pattern(pattern, element)
+          end
+          value = value.first if value.length == 1
+        end
+
+        [weight, value]
+      end
+    end
+
+    def sort_by(projected, direction)
       asc_sorter = lambda { |a, b| a.first <=> b.first }.to_proc
       desc_sorter = lambda { |a, b| b.first <=> a.first }.to_proc
 
@@ -35,29 +60,15 @@ class MockRedis
             raise "Invalid direction '#{direction}'"
         end
 
-      projected = enumerable.map do |element|
-        weight = by ? lookup_from_pattern(by, element) : element
-        value = element
-
-        if get_patterns.length > 0
-          value = get_patterns.map do |pattern|
-            pattern == "#" ? element : lookup_from_pattern(pattern, element)
-          end
-          value = value.first if value.length == 1
-        end
-
-        [weight, value]
-      end.sort(&sorter).map(&:last)
-
-      skip = limit.first || 0
-      take = limit.last || projected.length
-
-      sliced = projected[skip...(skip + take)] || projected
-
-      store ? rpush(store, sliced) : sliced
+      projected.sort(&sorter).map(&:last)
     end
 
-    private
+    def slice(sorted, limit)
+      skip = limit.first || 0
+      take = limit.last || sorted.length
+
+      sorted[skip...(skip + take)] || sorted
+    end
 
     def lookup_from_pattern(pattern, element)
       key = pattern.sub('*', element)
