4680fb243068fe82ff1815a9fc19af3a6b747452
Tyson Tate <tyson@tysontate.com>
2013-02-20 21:52:27 -0800

    Support Redis 2.6.10 INFO command fully.
    I tried to keep the old INFO values where possible for
    backward-compatibility's sake.

--- spec/commands/info_spec.rb
+++ spec/commands/info_spec.rb
@@ -1,13 +1,62 @@
 require 'spec_helper'
 
 describe '#info [mock only]' do
-  before { @info = @redises.mock.info }
+  let(:redis) { @redises.mock }
 
   it "responds with a config hash" do
-    @info.should be_a(Hash)
+    redis.info.should be_a(Hash)
   end
 
-  it "has some data in it" do
-    @info.keys.length.should > 0
+  it "gets default set of info" do
+    info = redis.info
+    info["arch_bits"].should be_a(String)
+    info["connected_clients"].should be_a(String)
+    info["aof_rewrite_scheduled"].should be_a(String)
+    info["used_cpu_sys"].should be_a(String)
+  end
+
+  it "gets all info" do
+    info = redis.info(:all)
+    info["arch_bits"].should be_a(String)
+    info["connected_clients"].should be_a(String)
+    info["aof_rewrite_scheduled"].should be_a(String)
+    info["used_cpu_sys"].should be_a(String)
+    info["cmdstat_slowlog"].should be_a(String)
+  end
+
+  it "gets server info" do
+    redis.info(:server)["arch_bits"].should be_a(String)
+  end
+
+  it "gets clients info" do
+    redis.info(:clients)["connected_clients"].should be_a(String)
+  end
+
+  it "gets memory info" do
+    redis.info(:memory)["used_memory"].should be_a(String)
+  end
+
+  it "gets persistence info" do
+    redis.info(:persistence)["aof_rewrite_scheduled"].should be_a(String)
+  end
+
+  it "gets stats info" do
+    redis.info(:stats)["keyspace_hits"].should be_a(String)
+  end
+
+  it "gets replication info" do
+    redis.info(:replication)["role"].should be_a(String)
+  end
+
+  it "gets cpu info" do
+    redis.info(:cpu)["used_cpu_sys"].should be_a(String)
+  end
+
+  it "gets keyspace info" do
+    redis.info(:keyspace)["db0"].should be_a(String)
+  end
+
+  it "gets commandstats info" do
+    redis.info(:commandstats)["sunionstore"]["usec"].should be_a(String)
   end
 end
