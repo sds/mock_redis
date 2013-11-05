5b3bf8190ebc0aa9aa8c714cb0423c58f8241a73
tomykaira <tomykaira@gmail.com>
2013-11-05 20:53:54 +0900

    Support :nx, :xx, :ex, :px options for SET command
    Based on:
    
      https://github.com/causes/mock_redis/pull/71
    
    with minor tweaks.
    
    Signed-off-by: Greg Hurrell <greg@causes.com>
    Change-Id: I6cc1fe8544a02e8ce673ef9fa6154c7a61ca6112
    Reviewed-on: https://gerrit.causes.com/30771
    Tested-by: jenkins <jenkins@causes.com>

--- spec/commands/set_spec.rb
+++ spec/commands/set_spec.rb
@@ -4,4 +4,60 @@ describe '#set(key, value)' do
   it "responds with 'OK'" do
     @redises.set('mock-redis-test', 1).should == 'OK'
   end
+
+  context 'options' do
+    it 'raises an error for EX seconds = 0' do
+      expect do
+        @redises.set('mock-redis-test', 1, ex: 0)
+      end.to raise_error(Redis::CommandError, 'ERR invalid expire time in SETEX')
+    end
+
+    it 'raises an error for PX milliseconds = 0' do
+      expect do
+        @redises.set('mock-redis-test', 1, px: 0)
+      end.to raise_error(Redis::CommandError, 'ERR invalid expire time in SETEX')
+    end
+
+    it 'accepts NX' do
+      key = 'mock-redis-test'
+      @redises.del(key)
+      @redises.set(key, 1, nx: true).should be_true
+      @redises.set(key, 1, nx: true).should be_false
+    end
+
+    it 'accepts XX' do
+      key = 'mock-redis-test'
+      @redises.del(key)
+      @redises.set(key, 1, xx: true).should be_false
+      @redises.set(key, 1).should == 'OK'
+      @redises.set(key, 1, xx: true).should be_true
+    end
+
+    context '[mock only]' do
+      before(:all) do
+        @mock = @redises.mock
+      end
+
+      before do
+        @now = Time.now
+        Time.stub!(:now).and_return(@now)
+      end
+
+      it 'accepts EX seconds' do
+        key = 'mock-redis-test'
+        @mock.set(key, 1, ex: 1).should == 'OK'
+        @mock.get(key).should_not be_nil
+        Time.stub!(:now).and_return(@now + 2)
+        @mock.get(key).should be_nil
+      end
+
+      it 'accepts PX milliseconds' do
+        key = 'mock-redis-test'
+        @mock.set(key, 1, px: 1000).should == 'OK'
+        @mock.get(key).should_not be_nil
+        Time.stub!(:now).and_return(@now + 2)
+        @mock.get(key).should be_nil
+      end
+    end
+  end
 end
