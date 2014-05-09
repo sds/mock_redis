dfc113c40e124c9a407a64301b3426d2fee5ee96
Ryan Biesemeyer <ryan@simplymeasured.com>
2014-05-09 17:35:40 +0000

    smembers: don't return frozen elements

--- spec/commands/smembers_spec.rb
+++ spec/commands/smembers_spec.rb
@@ -14,5 +14,15 @@ describe '#smembers(key)' do
     @redises.smembers(@key).should == %w[Test World Hello]
   end
 
+  it 'returns unfrozen copies of the input' do
+    input = 'a string'
+    @redises.sadd(@key, input)
+    output = @redises.smembers(@key).first
+
+    expect(output).to eq input
+    expect(output).to_not equal input
+    expect(output).to_not be_frozen
+  end
+
   it_should_behave_like "a set-only command"
 end
