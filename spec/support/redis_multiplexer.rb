b5b21c6dc714e53a3229aae8b577dea1a1f7b6e6
Shane da Silva <shane@dasilva.io>
2015-07-03 17:59:45 -0700

    Fix LineConcatenation RuboCop warnings
    Using `rubocop --only Style/LineConcatenation --auto-correct`

--- spec/support/redis_multiplexer.rb
+++ spec/support/redis_multiplexer.rb
@@ -37,24 +37,24 @@ class RedisMultiplexer < BlankSlate
     elsif (!equalish?(mock_retval, real_retval) && !mock_error && !real_error)
       # no exceptions, just different behavior
       raise MismatchedResponse,
-        "Mock failure: responses not equal.\n" +
-        "Redis.#{method}(#{args.inspect}) returned #{real_retval.inspect}\n" +
+        "Mock failure: responses not equal.\n" \
+        "Redis.#{method}(#{args.inspect}) returned #{real_retval.inspect}\n" \
         "MockRedis.#{method}(#{args.inspect}) returned #{mock_retval.inspect}\n"
     elsif (!mock_error && real_error)
       raise MismatchedResponse,
-        "Mock failure: didn't raise an error when it should have.\n" +
-        "Redis.#{method}(#{args.inspect}) raised #{real_error.inspect}\n" +
-        "MockRedis.#{method}(#{args.inspect}) raised nothing " +
+        "Mock failure: didn't raise an error when it should have.\n" \
+        "Redis.#{method}(#{args.inspect}) raised #{real_error.inspect}\n" \
+        "MockRedis.#{method}(#{args.inspect}) raised nothing " \
         "and returned #{mock_retval.inspect}"
     elsif (!real_error && mock_error)
       raise MismatchedResponse,
-        "Mock failure: raised an error when it shouldn't have.\n" +
-        "Redis.#{method}(#{args.inspect}) returned #{real_retval.inspect}\n" +
+        "Mock failure: raised an error when it shouldn't have.\n" \
+        "Redis.#{method}(#{args.inspect}) returned #{real_retval.inspect}\n" \
         "MockRedis.#{method}(#{args.inspect}) raised #{mock_error.inspect}"
     elsif (mock_error && real_error && !equalish?(mock_error, real_error))
       raise MismatchedResponse,
-        "Mock failure: raised the wrong error.\n" +
-        "Redis.#{method}(#{args.inspect}) raised #{real_error.inspect}\n" +
+        "Mock failure: raised the wrong error.\n" \
+        "Redis.#{method}(#{args.inspect}) raised #{real_error.inspect}\n" \
         "MockRedis.#{method}(#{args.inspect}) raised #{mock_error.inspect}"
     end
 
