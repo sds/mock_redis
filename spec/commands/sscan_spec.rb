55e7ba4210079d516a43a5d5b24ce471c1c67eb9
Ryan Showalter <ryan.showalter@gearboxsoftware.com>
2016-01-11 16:26:52 -0600

    Monkey patch sscan on all Redis/MockRedis objects to sort results

--- spec/commands/sscan_spec.rb
+++ spec/commands/sscan_spec.rb
@@ -5,6 +5,18 @@ describe '#sscan' do
   let(:match) { '*' }
   let(:key) { 'mock-redis-test:sscan' }
 
+  before do
+    # The return order of the members is non-deterministic, so we sort them to compare
+    expect_any_instance_of(Redis).to receive(:sscan).and_wrap_original do |m, *args|
+      result = m.call(*args)
+      [result[0], result[1].sort]
+    end
+    expect_any_instance_of(MockRedis).to receive(:sscan).and_wrap_original do |m, *args|
+      result = m.call(*args)
+      [result[0], result[1].sort]
+    end
+  end
+
   context 'when the set does not exist' do
     it 'returns a 0 cursor and an empty collection' do
       expect(@redises.sscan(key, 0, count: count, match: match)).to eq(['0', []])
@@ -18,7 +30,7 @@ describe '#sscan' do
       @redises.sadd(key, 'Test')
     end
 
-    let(:expected) { ['0', %w[Test World Hello]] }
+    let(:expected) { ['0', %w[Hello Test World]] }
 
     it 'returns a 0 cursor and the collection' do
       expect(@redises.sscan(key, 0, count: count)).to eq(expected)
