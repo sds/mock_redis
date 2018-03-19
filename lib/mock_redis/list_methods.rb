8a8563544c8f567b4f070b2f67ceae31d36fa0ca
Chad Simmons <polpak@gmail.com>
2018-03-19 12:32:37 -0700

    Fix rpushx to allow an array with multiple values

--- lib/mock_redis/list_methods.rb
+++ lib/mock_redis/list_methods.rb
@@ -170,7 +170,7 @@ class MockRedis
 
     def rpushx(key, value)
       value = [value] unless value.is_a?(Array)
-      if value.length != 1
+      if value.empty?
         raise Redis::CommandError, "ERR wrong number of arguments for 'rpushx' command"
       end
       assert_listy(key)
