d554f7d8e42546b00f80b50f4b733cc5d66e7368
Joseph Haig <josephhaig@gmail.com>
2018-06-09 22:02:05 +0100

    Rubocop

--- spec/commands/xrange_spec.rb
+++ spec/commands/xrange_spec.rb
@@ -6,8 +6,7 @@ describe '#xrange(key, start, end)' do
   it 'finds a single entry with a full range' do
     @redises.xadd(@key, '1234567891234-0', 'key', 'value')
     expect(@redises.xrange(@key, '-', '+'))
-       .to eq [ [ '1234567891234-0', [ 'key', 'value' ] ] ]
-
+      .to eq [['1234567891234-0', %w[key value]]]
   end
 
   context 'six items on the list' do
@@ -23,12 +22,12 @@ describe '#xrange(key, start, end)' do
     it 'returns entries in sequential order' do
       expect(@redises.xrange(@key, '-', '+')).to eq(
         [
-          [ '1234567891234-0', [ 'key1', 'value1' ] ],
-          [ '1234567891245-0', [ 'key2', 'value2' ] ],
-          [ '1234567891245-1', [ 'key3', 'value3' ] ],
-          [ '1234567891278-0', [ 'key4', 'value4' ] ],
-          [ '1234567891278-1', [ 'key5', 'value5' ] ],
-          [ '1234567891299-0', [ 'key6', 'value6' ] ]
+          ['1234567891234-0', %w[key1 value1]],
+          ['1234567891245-0', %w[key2 value2]],
+          ['1234567891245-1', %w[key3 value3]],
+          ['1234567891278-0', %w[key4 value4]],
+          ['1234567891278-1', %w[key5 value5]],
+          ['1234567891299-0', %w[key6 value6]]
         ]
       )
     end
@@ -36,11 +35,11 @@ describe '#xrange(key, start, end)' do
     it 'returns entries with a lower limit' do
       expect(@redises.xrange(@key, '1234567891239-0', '+')).to eq(
         [
-          [ '1234567891245-0', [ 'key2', 'value2' ] ],
-          [ '1234567891245-1', [ 'key3', 'value3' ] ],
-          [ '1234567891278-0', [ 'key4', 'value4' ] ],
-          [ '1234567891278-1', [ 'key5', 'value5' ] ],
-          [ '1234567891299-0', [ 'key6', 'value6' ] ]
+          ['1234567891245-0', %w[key2 value2]],
+          ['1234567891245-1', %w[key3 value3]],
+          ['1234567891278-0', %w[key4 value4]],
+          ['1234567891278-1', %w[key5 value5]],
+          ['1234567891299-0', %w[key6 value6]]
         ]
       )
     end
@@ -48,11 +47,11 @@ describe '#xrange(key, start, end)' do
     it 'returns entries with an upper limit' do
       expect(@redises.xrange(@key, '-', '1234567891285-0')).to eq(
         [
-          [ '1234567891234-0', [ 'key1', 'value1' ] ],
-          [ '1234567891245-0', [ 'key2', 'value2' ] ],
-          [ '1234567891245-1', [ 'key3', 'value3' ] ],
-          [ '1234567891278-0', [ 'key4', 'value4' ] ],
-          [ '1234567891278-1', [ 'key5', 'value5' ] ]
+          ['1234567891234-0', %w[key1 value1]],
+          ['1234567891245-0', %w[key2 value2]],
+          ['1234567891245-1', %w[key3 value3]],
+          ['1234567891278-0', %w[key4 value4]],
+          ['1234567891278-1', %w[key5 value5]]
         ]
       )
     end
@@ -60,10 +59,10 @@ describe '#xrange(key, start, end)' do
     it 'returns entries with both a lower and an upper limit' do
       expect(@redises.xrange(@key, '1234567891239-0', '1234567891285-0')).to eq(
         [
-          [ '1234567891245-0', [ 'key2', 'value2' ] ],
-          [ '1234567891245-1', [ 'key3', 'value3' ] ],
-          [ '1234567891278-0', [ 'key4', 'value4' ] ],
-          [ '1234567891278-1', [ 'key5', 'value5' ] ]
+          ['1234567891245-0', %w[key2 value2]],
+          ['1234567891245-1', %w[key3 value3]],
+          ['1234567891278-0', %w[key4 value4]],
+          ['1234567891278-1', %w[key5 value5]]
         ]
       )
     end
