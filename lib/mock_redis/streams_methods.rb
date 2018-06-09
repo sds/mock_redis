a7cebfc10241dbe4d22f694b2fce2bdc286a4ef3
Joseph Haig <josephhaig@gmail.com>
2018-06-09 11:25:27 +0100

    Id in time requires milliseconds

--- lib/mock_redis/streams_methods.rb
+++ lib/mock_redis/streams_methods.rb
@@ -1,7 +1,9 @@
+require 'date'
+
 class MockRedis
   module StreamsMethods
     def xadd(key, id, *args)
-      return "#{Time.now.to_i}-0" if id == '*'
+      return "#{DateTime.now.strftime('%Q')}-0" if id == '*'
     end
   end
 end
