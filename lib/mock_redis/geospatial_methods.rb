5606ebe26ee340720ce6b14b1aba153f918d6099
Ruslan Kotov <rkotov93@gmail.com>
2018-06-19 18:44:08 +0200

    Add #geopos implementation

--- lib/mock_redis/geospatial_methods.rb
+++ lib/mock_redis/geospatial_methods.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class MockRedis
   module GeospatialMethods
     LNG_RANGE = (-180..180)
@@ -8,11 +10,24 @@ class MockRedis
       points = parse_points(args)
 
       scored_points = points.map do |point|
-        score = calculate_zset_score(point[:lng], point[:lat])
+        score = encode(point[:lng], point[:lat])
         [score.to_s, point[:key]]
       end
 
       zadd(key, scored_points)
+    end
+
+    def geopos(key, *args)
+      return [] if zcard(key).zero?
+
+      args.map do |member|
+        score = zscore(key, member)&.to_i
+        next nil unless score
+        lng, lat = decode(score)
+        lng = format_decoded_coord(lng)
+        lat = format_decoded_coord(lat)
+        [lng, lat]
+      end
     end
 
     private
@@ -46,7 +61,8 @@ class MockRedis
       raise Redis::CommandError, 'ERR value is not a valid float'
     end
 
-    def calculate_zset_score(lng, lat)
+    # Returns ZSET score for passed coordinates
+    def encode(lng, lat)
       lat_offset = (lat - LAT_RANGE.min) / (LAT_RANGE.max - LAT_RANGE.min)
       lng_offset = (lng - LNG_RANGE.min) / (LNG_RANGE.max - LNG_RANGE.min)
 
@@ -77,6 +93,65 @@ class MockRedis
       y = (y | (y << s[0])) & b[0]
 
       x | (y << 1)
+    end
+
+    # Decodes ZSET score to coordinates pair
+    def decode(bits)
+      hash_sep = deinterleave(bits)
+
+      lat_scale = LAT_RANGE.max - LAT_RANGE.min
+      lng_scale = LNG_RANGE.max - LNG_RANGE.min
+
+      ilato = hash_sep & 0xFFFFFFFF # cast int64 to int32 to get lat part of deinterleaved hash
+      ilngo = hash_sep >> 32        # shift over to get lng part of hash
+
+      # Calculate approximate area
+      lat_min =  LAT_RANGE.min + (ilato * 1.0 / (1 << STEP)) * lat_scale
+      lat_max =  LAT_RANGE.min + ((ilato + 1) * 1.0 / (1 << STEP)) * lat_scale
+      lng_min =  LNG_RANGE.min + (ilngo * 1.0 / (1 << STEP)) * lng_scale
+      lng_max =  LNG_RANGE.min + ((ilngo + 1) * 1.0 / (1 << STEP)) * lng_scale
+
+      lng = (lng_min + lng_max) / 2
+      lat = (lat_min + lat_max) / 2
+
+      [lng, lat]
+    end
+
+    def deinterleave(bits)
+      b = [0x5555555555555555, 0x3333333333333333, 0x0F0F0F0F0F0F0F0F,
+           0x00FF00FF00FF00FF, 0x0000FFFF0000FFFF, 0x00000000FFFFFFFF]
+      s = [0, 1, 2, 4, 8, 16]
+
+      x = bits
+      y = bits >> 1
+
+      x = (x | (x >> s[0])) & b[0]
+      y = (y | (y >> s[0])) & b[0]
+
+      x = (x | (x >> s[1])) & b[1]
+      y = (y | (y >> s[1])) & b[1]
+
+      x = (x | (x >> s[2])) & b[2]
+      y = (y | (y >> s[2])) & b[2]
+
+      x = (x | (x >> s[3])) & b[3]
+      y = (y | (y >> s[3])) & b[3]
+
+      x = (x | (x >> s[4])) & b[4]
+      y = (y | (y >> s[4])) & b[4]
+
+      x = (x | (x >> s[5])) & b[5]
+      y = (y | (y >> s[5])) & b[5]
+
+      x | (y << 32)
+    end
+
+    def format_decoded_coord(coord)
+      coord = format('%.17f', coord)
+      l = 1
+      l += 1 while coord[-l] == '0'
+      coord = coord[0..-l]
+      coord[-1] == '.' ? coord[0..-2] : coord
     end
   end
 end
