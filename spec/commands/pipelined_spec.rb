9cfb410e685c131a092c36c39082b8cd31c50df1
gian <snmgian@gmail.com>
2018-09-26 18:23:31 -0300

    Fix pipelined array replies
    Array replies were being flatted, when the result of #pipelined
    should be an array of the values returned by each individual
    operation.

--- spec/commands/pipelined_spec.rb
+++ spec/commands/pipelined_spec.rb
@@ -39,4 +39,30 @@ describe '#pipelined' do
       future.class.should be MockRedis::Future
     end
   end
+
+  context 'with pipelined operations returning array replies' do
+    let(:key1) { 'colors' }
+    let(:key2) { 'seasons' }
+    let(:value1) { %w[blue yellow] }
+    let(:value2) { %w[summer winter] }
+
+    before do
+      @redises.rpush(key1, value1)
+      @redises.rpush(key2, value2)
+    end
+
+    after do
+      @redises.del key1
+      @redises.del key2
+    end
+
+    it 'returns an array of the array replies' do
+      results = @redises.pipelined do |redis|
+        @redises.lrange(key1, 0, -1)
+        @redises.lrange(key2, 0, -1)
+      end
+
+      results.should == [value1, value2]
+    end
+  end
 end
