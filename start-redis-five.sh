eff983685ee974068fea216086919cddad1e183d
Joe Haig <joseph.haig@arnoldclark.com>
2018-11-21 16:20:47 +0000

    Use system supplied version of Redis again

--- start-redis-five.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-set -ex
-
-wget https://github.com/antirez/redis/archive/5.0-rc4.tar.gz -O /tmp/redis-5.0-rc4.tar.gz
-tar -xvf /tmp/redis-5.0-rc4.tar.gz
-cd redis-5.0-rc4 && make
-./src/redis-server --daemonize yes
