1700e3c308ee462b4a3699c5837b16ebfb73028e
Pavel Slabý <pavel.slaby@devcast.cz>
2019-03-05 14:20:39 +0100

    Add possibility to call mset with Array

--- spec/commands/mset_spec.rb
+++ spec/commands/mset_spec.rb
@@ -10,6 +10,14 @@ describe '#mset(key, value [, key, value, ...])' do
     @redises.mset(@key1, 1).should == 'OK'
   end
 
+  it "responds with 'OK' for passed Array with 1 item" do
+    @redises.mset([@key1, 1]).should == 'OK'
+  end
+
+  it "responds with 'OK' for passed Array with 2 items" do
+    @redises.mset([@key1, 1, @key2, 2]).should == 'OK'
+  end
+
   it 'sets the values' do
     @redises.mset(@key1, 'value1', @key2, 'value2')
     @redises.mget(@key1, @key2).should == %w[value1 value2]
@@ -24,6 +32,12 @@ describe '#mset(key, value [, key, value, ...])' do
   it 'raises an error if given 0 arguments' do
     lambda do
       @redises.mset
+    end.should raise_error(Redis::CommandError)
+  end
+
+  it 'raises an error if given odd-sized array' do
+    lambda do
+      @redises.mset([@key1, 1, @key2])
     end.should raise_error(Redis::CommandError)
   end
 end
