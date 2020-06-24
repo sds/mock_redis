0997dad3725713a50dd07efb19b90d383d0cacf0
Thomas, Jason <jmthomas@ball.com>
2020-06-24 09:36:26 -0600

    Rubocop

--- lib/mock_redis/stream.rb
+++ lib/mock_redis/stream.rb
@@ -37,11 +37,11 @@ class MockRedis
     def trim(count)
       deleted = @members.size - count
       if deleted > 0
-        if count == 0
-          @members = Set.new
-        else
-          @members = @members.to_a[-count..-1].to_set
-        end
+        @members = if count == 0
+                     Set.new
+                   else
+                     @members.to_a[-count..-1].to_set
+                   end
         deleted
       else
         0
