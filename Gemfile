sds/mock_redis

commit 5393610f6fefe407dcb918ca87535f0c35da954e

Fix keyword arguments and stream specs (#199)

Gemfile
@@ -9,4 +9,6 @@ gem 'overcommit', '0.53.0'
 # Pin tool versions (which are executed by Overcommit) for Travis builds
 gem 'rubocop', '0.82.0'
 
+gem 'ruby2_keywords'
+
 gem 'coveralls', require: false

lib/mock_redis.rb
@@ -84,7 +84,7 @@ class MockRedis
     super || @db.respond_to?(method, include_private)
   end
 
-  def method_missing(method, *args, &block)
+  ruby2_keywords def method_missing(method, *args, &block)
     @db.send(method, *args, &block)
   end
 

lib/mock_redis/expire_wrapper.rb
@@ -12,7 +12,7 @@ class MockRedis
       @db = db
     end
 
-    def method_missing(method, *args, &block)
+    ruby2_keywords def method_missing(method, *args, &block)
       @db.expire_keys
       @db.send(method, *args, &block)
     end

lib/mock_redis/geospatial_methods.rb
@@ -12,7 +12,7 @@ class MockRedis
     D_R = Math::PI / 180.0
     EARTH_RADIUS_IN_METERS = 6_372_797.560856
 
-    def geoadd(key, *args)
+    ruby2_keywords def geoadd(key, *args)
       points = parse_points(args)
 
       scored_points = points.map do |point|

lib/mock_redis/multi_db_wrapper.rb
@@ -17,7 +17,7 @@ class MockRedis
       super || current_db.respond_to?(method, include_private)
     end
 
-    def method_missing(method, *args, &block)
+    ruby2_keywords def method_missing(method, *args, &block)
       current_db.send(method, *args, &block)
     end
 

lib/mock_redis/pipelined_wrapper.rb
@@ -18,7 +18,7 @@ class MockRedis
       @pipelined_futures = @pipelined_futures.clone
     end
 
-    def method_missing(method, *args, &block)
+    ruby2_keywords def method_missing(method, *args, &block)
       if in_pipeline?
         future = MockRedis::Future.new([method, *args], block)
         @pipelined_futures << future

lib/mock_redis/transaction_wrapper.rb
@@ -15,7 +15,7 @@ class MockRedis
       @multi_block_given = false
     end
 
-    def method_missing(method, *args, &block)
+    ruby2_keywords def method_missing(method, *args, &block)
       if in_multi?
         future = MockRedis::Future.new([method, *args], block)
         @transaction_futures << future

spec/commands/del_spec.rb
@@ -6,7 +6,9 @@ describe '#del(key [, key, ...])' do
   end
 
   before :each do
-    @redises._gsub(/\d{3}-\d/, '...-.')
+    # TODO: Redis appears to be returning a timestamp a few seconds in the future
+    # so we're ignoring the last 5 digits (time in milliseconds)
+    @redises._gsub(/\d{5}-\d/, '...-.')
   end
 
   it 'returns the number of keys deleted' do

commit 630490206571b985aa00f78538421d0ae8676a62

Include ruby2_keywords gem in gemspec
This fixes issues introduced by 5393610f.

Gemfile
@@ -9,6 +9,4 @@ gem 'overcommit', '0.53.0'
 # Pin tool versions (which are executed by Overcommit) for Travis builds
 gem 'rubocop', '0.82.0'
 
-gem 'ruby2_keywords'
-
 gem 'coveralls', require: false

mock_redis.gemspec
@@ -26,5 +26,6 @@ Gem::Specification.new do |s|
   s.add_development_dependency 'redis', '~> 4.2.0'
   s.add_development_dependency 'rspec', '~> 3.0'
   s.add_development_dependency 'rspec-its', '~> 1.0'
+  s.add_development_dependency 'ruby2_keywords'
   s.add_development_dependency 'timecop', '~> 0.9.1'
 end