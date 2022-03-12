02fb6a2f35d7f157e338dc91de3ed6e566afaee3
Thomas, Jason M (Software) <jmthomas@ball.com>
2022-03-12 11:23:05 -0700

    Fix quotes

--- spec/commands/ping_spec.rb
+++ spec/commands/ping_spec.rb
@@ -1,11 +1,11 @@
 require 'spec_helper'
 
 describe '#ping' do
-  it "returns 'PONG with no arguments'" do
+  it 'returns "PONG" with no arguments' do
     @redises.ping.should == 'PONG'
   end
 
-  it "returns the argument" do
+  it 'returns the argument' do
     @redises.ping('HELLO').should == 'HELLO'
   end
 end
