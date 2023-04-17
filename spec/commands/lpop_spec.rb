97a9f9224e0462e583bcea42e36ecaab52ea9be1
Theodore Hanson <ted@avochato.com>
2023-04-17 16:00:00 -0700

    feat(add_lpop_count): Add third arg to lpop to allow popping multiple elements as defined here: https://redis.io/commands/lpop/

--- spec/commands/lpop_spec.rb
+++ spec/commands/lpop_spec.rb
@@ -30,6 +30,37 @@ RSpec.describe '#lpop(key)' do
     expect(@redises.get(@key)).to be_nil
   end
 
+  context 'requesting multiple records to be popped in the 3rd arg' do
+    let(:count) { 10 }
+
+    before { count.times { @redises.lpush(@key, _1) } }
+
+    it 'returns the number of records requested' do
+      expect(@redises.lpop(@key, count)).to eq((count - 1).downto(0).map(&:to_s))
+    end
+
+    context 'when there are no records' do
+      let(:count) { 0 }
+      it 'returns nil' do
+        expect(@redises.lpop(@key, 10)).to eq(nil)
+      end
+    end
+
+    context 'when there are more records than requested' do
+      let(:count) { 20 }
+
+      it 'returns the most recent 10 records' do
+        expect(@redises.lpop(@key, 10)).to eq((count - 1).downto(10).map(&:to_s))
+      end
+    end
+
+    context 'when requesting more records than exist' do
+      it 'should return only the valid records in the bucket' do
+        expect(@redises.lpop(@key, 1_000_000)).to eq((count - 1).downto(0).map(&:to_s))
+      end
+    end
+  end
+
   let(:default_error) { RedisMultiplexer::MismatchedResponse }
   it_should_behave_like 'a list-only command'
 end
